f82956073f57038cbd6e7a3d3014e433ada7b76f
unknown57030 <unknown57030@gmail.com>
2026-02-27 21:06:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 const fs = require("fs");
 
 const app = express();
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
